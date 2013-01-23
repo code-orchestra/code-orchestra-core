bd6987e6822aa81ec1e316f98a5eee9a797345f1
Anton-I-Neverov <Anton.I.Neverov@gmail.com>
2013-01-24 02:50:15 +0700

    [CO-5080] ActualArgumentInfo fix

--- falcon/src/codeOrchestra/flex/processors/excludedNode/ActualArgumentInfoProcessor.java
+++ falcon/src/codeOrchestra/flex/processors/excludedNode/ActualArgumentInfoProcessor.java
@@ -3,6 +3,7 @@ package codeOrchestra.flex.processors.excludedNode;
 import codeOrchestra.flex.processors.SNodeProcessorException;
 import codeOrchestra.flex.tree.ActualArgumentInfoNode;
 import jetbrains.mps.smodel.SNode;
+import org.apache.flex.compiler.internal.tree.as.ExpressionNodeBase;
 import org.apache.flex.compiler.tree.as.IASNode;
 
 /**
@@ -10,13 +11,31 @@ import org.apache.flex.compiler.tree.as.IASNode;
  */
 public class ActualArgumentInfoProcessor extends ExcludedNodeProcessor {
 
+  private ExpressionNodeBase expression;
+
   public ActualArgumentInfoProcessor(SNode node) {
     super(node);
   }
 
   @Override
+  protected boolean handleChild(SNode child) throws SNodeProcessorException {
+    String childRole = child.getRole_();
+    if (childRole.equals("expression")) {
+      if (child.getChildCount() != 0) {
+        expression = processChild(child, ExpressionNodeBase.class);
+      }
+    } else {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
   protected IASNode createNode() throws SNodeProcessorException {
-    return new ActualArgumentInfoNode();
+    if (expression == null) {
+      return new ActualArgumentInfoNode();
+    }
+    return expression;
   }
 
 }
