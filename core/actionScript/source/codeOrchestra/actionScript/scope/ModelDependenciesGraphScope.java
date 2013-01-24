2b152d1fe63333a1538832a17b9f90a0d1426030
Alexander Eliseyev <a.a.eliseyev@gmail.com>
2013-01-24 18:13:05 +0700

    [CO-5111]

---
+++ core/actionScript/source/codeOrchestra/actionScript/scope/ModelDependenciesGraphScope.java
@@ -0,0 +1,145 @@
+package codeOrchestra.actionScript.scope;
+
+import jetbrains.mps.project.DevKit;
+import jetbrains.mps.project.IModule;
+import jetbrains.mps.project.structure.modules.ModuleReference;
+import jetbrains.mps.smodel.*;
+import jetbrains.mps.smodel.SModel.ImportElement;
+import jetbrains.mps.util.misc.hash.HashSet;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author: Alexander Eliseyev
+ */
+public class ModelDependenciesGraphScope implements IScope {
+
+  private Set<SModelReference> allModels = new HashSet<SModelReference>();
+  private Set<ModuleReference> allLanguages = new HashSet<ModuleReference>();
+
+  public ModelDependenciesGraphScope(SModelDescriptor scopeModel) {
+    initDependencies(scopeModel);
+  }
+
+  private void initDependencies(SModelDescriptor sModelDescriptor) {
+    SModelReference sModelReference = sModelDescriptor.getSModelReference();
+    if (allModels.contains(sModelReference)) {
+      return;
+    }
+
+    SModel sModel = sModelDescriptor.getSModel();
+
+    allModels.add(sModelReference);
+    allLanguages.addAll(sModel.importedLanguages());
+
+    // Recursion
+    for (ImportElement importElement : sModel.importedModels()) {
+      SModelDescriptor importedModelDescriptor = SModelRepository.getInstance().getModelDescriptor(importElement.getModelReference());
+      if (importedModelDescriptor != null) {
+        initDependencies(importedModelDescriptor);
+      }
+    }
+  }
+
+
+  @Override
+  public SModelDescriptor getModelDescriptor(SModelReference modelReference) {
+    if (allModels.contains(modelReference)) {
+      return SModelRepository.getInstance().getModelDescriptor(modelReference);
+    }
+    return null;
+  }
+
+  @Override
+  public Language getLanguage(ModuleReference moduleReference) {
+    if (allLanguages.contains(moduleReference)) {
+      return MPSModuleRepository.getInstance().getLanguage(moduleReference);
+    }
+    return null;
+  }
+
+  @Override
+  public Iterable<SModelDescriptor> getOwnModelDescriptors() {
+    return getModelDescriptors();
+  }
+
+  @Override
+  public DevKit getDevKit(ModuleReference ref) {
+    return null;
+  }
+
+  @Override
+  public Iterable<SModelDescriptor> getModelDescriptors() {
+    List<SModelDescriptor> result = new ArrayList<SModelDescriptor>();
+
+    for (SModelReference modelReference : allModels) {
+      SModelDescriptor modelDescriptor = SModelRepository.getInstance().getModelDescriptor(modelReference);
+      if (modelDescriptor != null) {
+        result.add(modelDescriptor);
+      }
+    }
+
+    return result;
+  }
+
+  @Override
+  public Iterable<Language> getVisibleLanguages() {
+    List<Language> result = new ArrayList<Language>();
+
+    for (ModuleReference langRef : allLanguages) {
+      Language language = MPSModuleRepository.getInstance().getLanguage(langRef);
+      if (language != null) {
+        result.add(language);
+      }
+    }
+
+    return result;
+  }
+
+  @Override
+  public Iterable<IModule> getVisibleModules() {
+    List<IModule> result = new ArrayList<IModule>();
+
+    for (SModelReference modelReference : allModels) {
+      SModelDescriptor modelDescriptor = SModelRepository.getInstance().getModelDescriptor(modelReference);
+      if (modelDescriptor != null) {
+        IModule module = modelDescriptor.getModule();
+        if (module != null) {
+          result.add(module);
+        }
+      }
+    }
+
+    return result;
+  }
+
+  @Override
+  public Iterable<DevKit> getVisibleDevkits() {
+    return Collections.EMPTY_LIST;
+  }
+
+  @Override
+  public SModelDescriptor getModelDescriptor(SModelFqName fqName) {
+    for (SModelReference modelReference : allModels) {
+      if (modelReference.getSModelFqName().equals(fqName)) {
+        return SModelRepository.getInstance().getModelDescriptor(modelReference);
+      }
+    }
+
+    return null;
+  }
+
+  @Override
+  public Language getLanguage(String fqName) {
+    for (ModuleReference languageReference : allLanguages) {
+      if (languageReference.getModuleFqName().equals(fqName)) {
+        return MPSModuleRepository.getInstance().getLanguage(languageReference);
+      }
+    }
+
+    return null;
+  }
+}
