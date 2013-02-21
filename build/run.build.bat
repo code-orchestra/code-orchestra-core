d85995e17f1220162ada44da7b02bc2dda3c091f
Anton-I-Neverov <Anton.I.Neverov@gmail.com>
2013-02-21 15:00:57 +0400

    changed Beta to RC1

--- build/run.build.bat
+++ build/run.build.bat
@@ -1,1 +1,1 @@
-ant -f MPS-external.xml -Dmps_home="C:\CodeOrchestra\code-orchestra-core" -Dversion="2.0 Beta" -Dbuild.vcs.number=555 -Dteamcity.buildConfName=CodeOrchestra
+ant -f MPS-external.xml -Dmps_home="C:\CodeOrchestra\code-orchestra-core" -Dversion="2.0 RC1" -Dbuild.vcs.number=555 -Dteamcity.buildConfName=CodeOrchestra
